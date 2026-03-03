cef5eb08010d1902a73f540fa35b357dd8537800
ljvdhooft <ljvdhooft@gmail.com>
2026-03-03 17:14:38 +0100

    fix: auto folding after exiting insert mode

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -92,14 +92,14 @@ return {
     event = "BufReadPost",
     config = function()
       -- Recommended settings
-      vim.o.foldcolumn = "1"
-      vim.o.foldlevel = 99
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
+      vim.opt.foldcolumn = "1"
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+      vim.opt.foldenable = true
 
       -- Use ufo provider
-      vim.o.foldmethod = "expr"
-      vim.o.foldexpr = "v:lua.require'ufo'.foldexpr()"
+      vim.opt.foldmethod = "manual"
+      vim.opt.foldexpr = "v:lua.require'ufo'.foldexpr()"
       -- vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep: ,foldinner: ,foldclose:'
 
       require("ufo").setup({
@@ -109,6 +109,10 @@ return {
       })
 
     end,
+    keys = {
+      { "zR", function() require("ufo").openAllFolds() end, desc = "Open all folds (ufo)" },
+      { "zM", function() require("ufo").closeAllFolds() end, desc = "Close all folds (ufo)" },
+    },
   },
   {
     "sindrets/diffview.nvim",
