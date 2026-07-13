158282e9e5529d175ae78337bafefe0af5605892
ljvdhooft <ljvdhooft@gmail.com>
2026-07-13 20:07:44 +0200

    feat: added markdown-preview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -181,6 +181,15 @@ return {
     },
   },
   {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = "markdown",
+    build = function() vim.fn["mkdp#util#install"]() end,
+    keys = {
+      { "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", desc = "Toggle Markdown Preview (browser)", ft = "markdown" },
+    },
+  },
+  {
     "ThePrimeagen/vim-be-good",
     cmd = "VimBeGood",
   },
